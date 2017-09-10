be00f9139b0f0d0b9d3c7d5d897bb7b33f01c753
a1liz <694426737@qq.com>
2017-09-10 16:26:24 +0800

    修改了介绍页面的侧边栏部分

--- 10web/application/views/team/detail.php
+++ 10web/application/views/team/detail.php
@@ -43,12 +43,16 @@ jQuery(window).load(function() {
 				</a>
 			</div>
 			<div class="hr3"></div>
-			<ul id="menu">
+			<!-- <ul id="menu">
 				<li class="current"><a href="#">关于亿灵</a></li>
 				<li><a href="#">智能设计部</a></li>
 				<li><a href="#">移动开发部</a></li>
 				<li><a href="#">网站运维部</a></li>
-			</ul>
+			</ul> -->
+			<div class="department first current"><img src="/assets/i/hafu_anti_logo_black.png" alt="亿灵" /><p>关于亿灵</p></div>
+			<div class="department"><img src="/assets/i/electric_car.png" alt="智能" /><p>智能设计部</p></div>
+			<div class="department"><img src="/assets/i/phone.png" alt="移动" /><p>移动开发部</p></div>
+			<div class="department"><img src="/assets/i/pc.png" alt="网站" /><p>网站运维部</p></div>
 	</div>
 	<div id="page">
 		<div id="wrapper">
@@ -64,12 +68,12 @@ jQuery(window).load(function() {
             </div>
         </div>
         </div>
-        <h3 class="center_title">亿灵软件联盟</h3>
+        <h3 class="center_title">亿灵软件开发联盟</h3>
         <div class="hr"></div>
         <ul class="list_num">
         	<li><span class="sp1">1</span><span class="sp2">智能设计部</span><br/>*--智能设计部简介--*</li>
-        	<li><span class="sp1">1</span><span class="sp2">移动开发部</span><br/>*--移动开发部简介--*</li>
-        	<li class="last"><span class="sp1">1</span><span class="sp2">网站运维部</span><br/>*--网站运维部简介--*</li>
+        	<li><span class="sp1">2</span><span class="sp2">移动开发部</span><br/>*--移动开发部简介--*</li>
+        	<li class="last"><span class="sp1">3</span><span class="sp2">网站运维部</span><br/>*--网站运维部简介--*</li>
         </ul>
         <div class="hr2"></div>
 	</div>
